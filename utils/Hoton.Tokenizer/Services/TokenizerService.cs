24ffc8b14d0d6540459742caf976c11bf4015a15
noah.hsu <xuhaojuntw@gmail.com>
2024-12-02 15:06:16 +0800

    feat(tokenizer): 斷詞結果加入每個字

--- utils/Hoton.Tokenizer/Services/TokenizerService.cs
+++ utils/Hoton.Tokenizer/Services/TokenizerService.cs
@@ -9,29 +9,9 @@ public class TokenizerService : ITokenizerService
 
     public IEnumerable<string> TokenizeForSearch(string text)
     {
-        var tokens = _segmenter.CutForSearch(text);
-
-        var result = new List<string>();
-
-        // ngram
-        foreach (var token in tokens)
-        {
-            if (decimal.TryParse(token, out _))
-            {
-                result.Add(token);
-            }
-            else
-            {
-                for (int i = 0; i < token.Length; i++)
-                {
-                    for (int j = i + 1; j <= token.Length; j++)
-                    {
-                        result.Add(token.Substring(i, j - i));
-                    }
-                }
-            }
-        }
-
-        return result;
+        var tokens = _segmenter.CutForSearch(text)
+            .SelectMany(token => token.Length == 1 ? [token] : token.Select(c => c.ToString()))
+            .Distinct();
+        return tokens;
     }
 }
